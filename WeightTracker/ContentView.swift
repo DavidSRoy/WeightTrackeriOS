4e798c2c858633a0568034651e7884b28b1d9933
David Roy <24599987+DavidSRoy@users.noreply.github.com>
2023-12-06 14:22:22 -0600

    Initial Commit

---
+++ WeightTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeightTracker
+//
+//  Created by David Roy on 12/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeightTracker/WeightTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightTrackerApp.swift
+//  WeightTracker
+//
+//  Created by David Roy on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WeightTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
